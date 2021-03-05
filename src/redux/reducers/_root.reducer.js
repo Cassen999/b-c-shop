717acb87a0da6a8b7cddc160c790123e95d21aaa
Bradley Service <bradleykservice@gmail.com>
2021-03-05 13:58:23 -0600

    billing address
    Co-authored-by: Cassen999 <Cassen999@users.noreply.github.com>

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import mailingAddressReducer from './mailingAddress.reducer';
+import billingAddressReducer from './billingAddress.reducer';
 
 const rootReducer = combineReducers({
   errors,
   user,
   mailingAddressReducer,
+  billingAddressReducer
 });
 
 export default rootReducer;
