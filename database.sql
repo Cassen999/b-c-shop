767223247e147aabcd3a1e1a9bfecd1aa6a198e8
Bradley Service <bradleykservice@gmail.com>
2021-03-09 12:40:12 -0600

    updated sql file

--- database.sql
+++ database.sql
@@ -7,31 +7,57 @@ CREATE TABLE "user" (
 INSERT INTO "user" ("username", "password")
 VALUES ('bradley', '$2a$10$O0lw0Jy8SbXo3l0Czv2Ss.P8ue42nxFEbKdQ/.DvFmNGfO.cUIGny'), ('user3', '$2a$10$O0lw0Jy8SbXo3l0Czv2Ss.P8ue42nxFEbKdQ/.DvFmNGfO.cUIGny'), ('user4', '$2a$10$O0lw0Jy8SbXo3l0Czv2Ss.P8ue42nxFEbKdQ/.DvFmNGfO.cUIGny');
 
-CREATE TABLE "categories" (
+CREATE TABLE "genres" (
 	"id" SERIAL PRIMARY KEY,
 	description VARCHAR (50)
 );
+INSERT INTO "genres" (description)
+VALUES ('Classical'), ('Electronic'), ('Rock'), ('Metal'), ('Jazz'), ('Blues');
 
-INSERT INTO "categories" (description)
-VALUES ('Kitchen'), ('Electronics'), ('Bath'), ('Movies'), ('Music'), ('Bed');
-
-CREATE TABLE "products" (
+CREATE TABLE "media-type" (
 	"id" SERIAL PRIMARY KEY,
-	p_name VARCHAR (80),
-	cat_id INT REFERENCES "categories" ON DELETE CASCADE,
+	description VARCHAR (50)
+);
+INSERT INTO "media-type" (description)
+VALUES ('CD'), ('Vinyl'), ('Cassette'), ('Digital');
+
+--CREATE TABLE "products" (
+--	"id" SERIAL PRIMARY KEY,
+--	p_name VARCHAR (80),
+--	cat_id INT REFERENCES "categories" ON DELETE CASCADE,
+--	price DECIMAL,
+--	stock INT
+--);
+
+CREATE TABLE "artists" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (100),
+	"founded" INT,
+	"city" VARCHAR (50),
+	"state" VARCHAR (50),
+	"country" VARCHAR (100)
+);
+INSERT INTO "artists" ("name", "founded", "city", "state", "country")
+VALUES ('Polyphia', '2010', 'Minneapolis', 'Minnesota', 'United States'), ('Korn', '1995', 'St. Louis', 'Missouri', 'United States');
+
+CREATE TABLE "albums" (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR (80),
+	artist_id INT REFERENCES "artists" ON DELETE CASCADE,
+	genre_id INT REFERENCES "genres" ON DELETE CASCADE,
+	type_id INT REFERENCES "media-type" ON DELETE CASCADE,
+	album_art BYTEA,
 	price DECIMAL,
 	stock INT
 );
-
-ALTER SEQUENCE products_id_seq RESTART WITH 1;
-
-INSERT INTO "products" (p_name, cat_id, price, stock)
-VALUES ('Knife', 1, 29.99, 10), ('Blender', 1, 59.99, 15), ('Dining Table', 1, 299.99, 5), ('iPhone', 2, 999.99, 7), ('PS5', 2, 399.99, 4), ('Laptop', 2, 799.99, 5), ('Toothbrush', 3, 3.99, 20), ('Toothpaste', 3, 4.99, 20), ('Shower Curtain', 3, 29.99, 5), ('Titanic', 4, 9.99, 3), ('Dodgeball', 4, 9.99, 2), ('The Matrix', 4, 7.99, 8), ('Electric Guitar', 5, 499.95, 2), ('Drumset', 5, 1299.89, 2), ('Bass Guitar', 5, 349.95, 1), ('Microphone', 5, 299.98, 3), ('Mattress', 6, 999.99, 2), ('Night Stand', 6, 89.90, 3), ('Blanket', 6, 47.99, 5);
+ALTER SEQUENCE albums_id_seq RESTART WITH 1;
+INSERT INTO "albums" ("name", artist_id, genre_id, type_id, price, stock)
+VALUES ('New Levels New Devils', 1, 4, 1, 10.99, 10), ('See You On The Other Side', 2, 3, 3, 29.99, 3);
 
 CREATE TABLE "cart" (
 	"id" SERIAL PRIMARY KEY,
 	user_id INT REFERENCES "user" ON DELETE CASCADE,
-	product_id INT REFERENCES "products" ON DELETE CASCADE
+	album_id INT REFERENCES "albums" ON DELETE CASCADE
 );
 
 CREATE TABLE "ship_address" (
