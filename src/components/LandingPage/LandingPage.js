ce5145f7ae3ac4917b119ad09e4a61353e594d1c
Cassen Gerber <cassenpt@gmail.com>
2021-03-10 15:12:51 -0600

    added material ui to dependencies, changed landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -31,15 +31,6 @@ class LandingPage extends Component {
               Fusce porta diam ac tortor elementum
             </p>
           </div>
-          <div className="grid-col grid-col_4">
-            <RegisterForm />
-            <center>
-              <h4>Already a Member?</h4>
-              <button className="btn btn_sizeSm" onClick={this.onLogin}>
-                Login
-              </button>
-            </center>
-          </div>
         </div>
       </div>
     );
