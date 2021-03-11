a492dad724e6b5ac868f1c2ec0d0d7044fd867c3
Cassen Gerber <cassenpt@gmail.com>
2021-03-11 11:33:28 -0600

    changed copyright

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
 import './LandingPage.css';
-import RegisterForm from '../RegisterForm/RegisterForm';
+import { withStyles, Grid } from '@material-ui/core';
 
 class LandingPage extends Component {
   state = {
@@ -14,27 +14,13 @@ class LandingPage extends Component {
   };
 
   render() {
+    const { classes } = this.props;
     return (
-      <div className="container">
-        <h2>{this.state.heading}</h2>
-        <div className="grid">
-          <div className="grid-col grid-col_8">
-            <p>
-              Lorem ipsum dolor sit amet
-            </p>
-
-            <p>
-              Praesent consectetur orci dui
-            </p>
-
-            <p>
-              Fusce porta diam ac tortor elementum
-            </p>
-          </div>
-        </div>
+      <div>
+        
       </div>
     );
   }
 }
 
-export default connect(mapStoreToProps)(LandingPage);
+export default connect(mapStoreToProps)(withStyles(styles)(LandingPage));
